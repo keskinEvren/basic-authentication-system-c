ee2ad3926c77ab6e7deb8438fd27812972d843ae
Evren Keskin <evrenkeskin0998@gmail.com>
2022-06-26 19:56:06 +0300

    makeUsername() added
    makeUsername() and operation screen added.

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct user{
+	char name[25];
+	char email[25];
+	char password[25];
+	char username[25];
+	char phoneNumber[25];
+};
 
-int main(int argc, char *argv[]) {
+void takeInput(char ch[25]){
+	fgets(ch, 25, stdin);
+	ch[strlen(ch)-1] = 0;
+}
+
+void makeUsername(char email[25], char username[25]){
+	for(int i = 0; i<strlen(email); i++){
+		if(email[i] = '@') break;
+		else username[i] = email[i];
+	}
+}
+
+
+int main() {
+	int action;
+	
+	printf("\t\t\t **** Welcome to Authentication System ****");
+	printf("\n Choose you action");
+	printf("\n (1)Login");
+	printf("\n (2)Signup");
+	printf("\n (3)Exit");
+	
+	printf("\n\n Your Choice : ");
+	scanf("%d", action);
+	fgetc(stdin);
+	
+	
 	return 0;
 }
