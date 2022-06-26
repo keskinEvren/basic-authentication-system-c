8fe127e90437d5c36dc799674873a56b674368c4
Evren Keskin <evrenkeskin0998@gmail.com>
2022-06-26 20:48:41 +0300

    some methods added.
    username generator and password matcher addad.

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 #include<conio.h>
 #include<windows.h>
+#define ENTER 13
+#define TAB 9
+#define BACKSPACE 8
 
 struct user{
 	char name[25];
 	char email[25];
 	char password[25];
 	char username[25];
-	char phoneNumber[25];
+	char phoneNumber[25];	
 };
 
 void takeInput(char ch[25]){
@@ -15,9 +18,30 @@ void takeInput(char ch[25]){
 	ch[strlen(ch)-1] = 0;
 }
 
-void makeUsername(char email[25], char username[25]){
+void takePassword(char pw[25]){
+	int i;
+	char ch;
+	while(1){
+		ch=getch();
+		if(ch == ENTER || ch == TAB){
+			pw[i] = '\0';
+			break;
+		}else if(ch == BACKSPACE){
+			if(i>0){
+				i--;
+				printf("");
+			}
+		}else{
+			pw[i++] = ch;
+			printf("* ");
+			
+		}
+	}
+}
+
+char makeUsername(char email[25], char username[25]){
 	for(int i = 0; i<strlen(email); i++){
-		if(email[i] = '@') break;
+		if(email[i] == '@') break;
 		else username[i] = email[i];
 	}
 }
@@ -25,6 +49,8 @@ void makeUsername(char email[25], char username[25]){
 
 int main() {
 	int action;
+	struct user user;
+	char passwordBackup[25];
 	
 	printf("\t\t\t **** Welcome to Authentication System ****");
 	printf("\n Choose you action");
@@ -33,8 +59,37 @@ int main() {
 	printf("\n (3)Exit");
 	
 	printf("\n\n Your Choice : ");
-	scanf("%d", action);
+	scanf("%d", &action);
 	fgetc(stdin);
+	
+	
+	switch(action){
+		case 1:
+			printf("\nEnter your name: ");
+			takeInput(user.name);
+			printf("\nEnter your email: ");
+			takeInput(user.email);
+			printf("\nEnter your phone number: ");
+			takeInput(user.phoneNumber);
+			printf("\nEnter your password: ");
+			takePassword(user.password);
+			printf("\nEnter your password: ");
+			takePassword(passwordBackup);
+			
+			if(!strcmp(user.password,passwordBackup)){
+				printf("\n\t Passwords Matched");
+				makeUsername(user.email, user.username);
+				printf("\n Your generated username is %s", user.username);
+			}else{
+				printf("\n\t Passwords Not Matched !!!");
+			}
+			
+	}
+	
+	
+	
+	
+	
 	
 	
 	return 0;
