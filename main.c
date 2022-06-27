29d14d19dc97ed2d361f47b8bf092826b41e56c8
Evren Keskin <evrenkeskin0998@gmail.com>
2022-06-27 13:24:27 +0300

    login options added

--- main.c
+++ main.c
@@ -48,9 +48,10 @@ char makeUsername(char email[25], char username[25]){
 
 
 int main() {
-	int action;
+	int action, userFound = 0;
 	struct user user;
 	char passwordBackup[25];
+	FILE *fp;
 	
 	printf("\t\t\t **** Welcome to Authentication System ****");
 	printf("\n Choose you action");
@@ -62,9 +63,13 @@ int main() {
 	scanf("%d", &action);
 	fgetc(stdin);
 	
+	char username[50];
+	char psword[25];
+	struct user userr;
+	
 	
 	switch(action){
-		case 1:
+		case 2:
 			printf("\nEnter your name: ");
 			takeInput(user.name);
 			printf("\nEnter your email: ");
@@ -80,17 +85,51 @@ int main() {
 				printf("\n\t Passwords Matched");
 				makeUsername(user.email, user.username);
 				printf("\n Your generated username is %s", user.username);
+				fp = fopen("user.dat","a+");
+				fwrite(&user,sizeof(struct user),1,fp);
+				if(fwrite != 0) printf("\n Your registration is success, ");
+				else printf("\n Something Went Wrong !!!");
+				fclose(fp);
 			}else{
 				printf("\n\t Passwords Not Matched !!!");
 			}
+		break;
+		
+		case 1:
 			
+			printf("\n Enter your username: ");
+			takeInput(username);
+			printf("\n Enter your password: ");
+			takeInput(psword);
+			
+			fp = fopen("user.dat","r");
+			while(fread(&userr, sizeof(struct user),1,fp)){
+				if(!strcmp(userr.username, username)){
+					if(!strcmp(userr.password,psword)){
+						printf("\n\n Login is Successfull ");
+						printf("\n\t\t Profile Informations");
+						printf("\n %s",userr.name);
+						printf("\n %s",userr.email);
+						printf("\n %s",userr.phoneNumber);
+					}else{
+						Beep(1000,500);
+						printf("\n Login is Unsuccessfull !!!");
+						printf("\n Check Your Informations");
+					}
+					userFound = 1;
+				}
+			}
+			if(!userFound){
+				printf("\n\n User is not Found Please Singup Before Login");
+				Beep(1000,500);
+			}
+			fclose(fp);
+			break;
+		
+		case 3:
+			printf("\t\t Good Bye :)");
+			return 0;
 	}
-	
-	
-	
-	
-	
-	
 	
 	return 0;
 }
